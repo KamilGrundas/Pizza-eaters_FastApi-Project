a438a9b79406a36a0c5ce0ece3eba3918140618c
koc90 <dabrowska.olga.90@gmail.com>
2024-02-28 20:27:30 +0100

    próba

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import uvicorn
 
 app = FastAPI()
 
+
 @app.get("/")
 def index():
     return {"msg": "Hello World"}
 
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+# komentarz dodany na próbę
