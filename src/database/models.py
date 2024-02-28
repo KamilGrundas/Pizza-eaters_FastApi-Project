0197c46a2a7d6b25272ce0f595cd62a682e6f6de
Kamil Grundas <kamil.grundas@gmail.com>
2024-02-28 19:56:09 +0100

    Initial commit

---
+++ src/database/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Boolean, func, Table
+from sqlalchemy.sql.sqltypes import DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column("crated_at", DateTime, default=func.now())
+    role = Column()
