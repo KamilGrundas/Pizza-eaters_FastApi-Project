aa1896df4ef35e3cc5b50186c7308182c7c34351
Kamil Grundas <kamil.grundas@gmail.com>
2024-03-02 15:17:30 +0100

    Load data from db in template

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from src.routes import fake_pictures
+from src.database.db import get_db
+from fastapi import Depends
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 app.include_router(fake_pictures.router, prefix="/wizards")
@@ -13,12 +16,15 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/login/", response_class=HTMLResponse)
-async def get_home(request: Request):
+async def login_page(request: Request):
     return templates.TemplateResponse("login_register.html", {"request": request})
 
 @app.get("/", response_class=HTMLResponse)
-async def get_home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+async def get_home(request: Request, db: Session = Depends(get_db)):
+    pictures =  await fake_pictures.display_pictures(db)
+    context = {"pictures" : pictures}
+    return templates.TemplateResponse("index.html", {"request": request, "context":context})
+    
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
