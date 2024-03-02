6ba267290848f7ee20888de61a80dc2fd6bcdb96
Kamil Grundas <kamil.grundas@gmail.com>
2024-03-02 14:47:36 +0100

    Small fix

--- main.py
+++ main.py
@@ -12,10 +12,13 @@ app.include_router(fake_pictures.router, prefix="/wizards")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+@app.get("/login/", response_class=HTMLResponse)
+async def get_home(request: Request):
+    return templates.TemplateResponse("login_register.html", {"request": request})
+
 @app.get("/", response_class=HTMLResponse)
 async def get_home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
