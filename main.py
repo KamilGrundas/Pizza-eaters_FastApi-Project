41e3f2f23ea14dc03aca78ffb7fd72a744af72f7
Kamil Grundas <kamil.grundas@gmail.com>
2024-03-10 15:45:55 +0100

    Added template login

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import uvicorn
-from fastapi import Request, APIRouter, HTTPException
+from fastapi import Request, APIRouter, HTTPException, Response
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from src.schemas import CommentBase, CommentResponse
 from src.repository import pictures as pictures_repo
 from src.database.db import get_db
 from src.schemas import PictureResponse
-
+from src.services.auth_new import get_current_user
 from fastapi import Depends, status
-
+from src.database.models import User
 from src.routes import auth, tags, comments, auth_new
 from src.routes import pictures as pict
-
+from typing import Optional
 from sqlalchemy.orm import Session
 from fastapi import FastAPI, File, UploadFile, Form
+from src.services.auth_new import create_access_token, create_refresh_token, verify_password
+from fastapi.security import OAuth2PasswordRequestForm
+from src.repository.users_new import get_user_by_email, update_token
+from src.services.cookie import AuthTokenMiddleware
 
 
 app = FastAPI()
@@ -25,15 +29,43 @@ app = FastAPI()
 app.include_router(pict.router, prefix="/wizards")
 app.include_router(tags.router, prefix="/wizards")
 app.include_router(auth_new.router, prefix="/api")
-
+app.add_middleware(AuthTokenMiddleware)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/login/", response_class=HTMLResponse)
-async def login_page(request: Request):
-    return templates.TemplateResponse("login_register.html", {"request": request})
+
+@app.get("/set-cookie")
+def set_cookie(response: Response):
+    response.set_cookie(key="mycookie", value="value_of_cookie")
+    return {"message": "Cookie has been set"}
+
+# @app.get("/login/", response_class=HTMLResponse)
+# async def login_page(request: Request):
+#     return templates.TemplateResponse("login_register.html", {"request": request})
+
+@app.post("/login")
+async def login(response: Response, body: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+
+    user = await get_user_by_email(body.username, db)
+    if user is None or not verify_password(body.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email, password, or email not confirmed"
+        )
+
+    access_token = await create_access_token(data={"sub": user.email})
+    refresh_token = await create_refresh_token(data={"sub": user.email})
+    await update_token(user, refresh_token, db)
+    
+    # Ustawienie ciasteczek z tokenami
+    response.set_cookie(key="access_token", value=f"{access_token}", httponly=True, samesite="None", secure=True)
+    response.set_cookie(key="refresh_token", value=refresh_token, httponly=True, samesite="None", secure=True)
+
+    # Przekierowanie po pomyślnym logowaniu
+    return {"message": "Cookie has been set"}
+
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -53,14 +85,31 @@ async def get_picture(
     picture_id: int,
     db: Session = Depends(get_db),
     comment_text: str = Form(None),
+    current_user: Optional[User] = Depends(get_current_user),  # Dodawanie opcjonalnego użytkownika
 ):
     picture = await pictures_repo.get_picture(picture_id, db)
-    context = {"picture": picture}
+    context = {
+        "picture": picture,
+        "user": current_user  # Przekazujesz użytkownika do kontekstu
+    }
+    print(current_user.id)
     if request.method == "POST" and comment_text:
-        await comments.add_new_comment(picture_id, CommentBase(text=comment_text), db)
+        # Przy dodawaniu komentarza, sprawdzasz, czy użytkownik jest zalogowany
+        if current_user:
+            await comments.add_new_comment(picture_id, CommentBase(text=comment_text), db,current_user)
+        else:
+            # Możesz tutaj obsłużyć sytuację, gdy użytkownik nie jest zalogowany
+            # na przykład przekierowując na stronę logowania lub wyświetlając komunikat
+            pass
     return templates.TemplateResponse(
         "picture.html", {"request": request, "context": context}
     )
+
+@app.get("/login", response_class=HTMLResponse)
+async def login_form(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
 
 
 if __name__ == "__main__":
--- src/services/auth_new.py
+++ src/services/auth_new.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 
 from jose import JWTError, jwt
-from fastapi import HTTPException, status, Depends
+from fastapi import HTTPException, status, Depends, Request
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
@@ -76,31 +76,31 @@ async def decode_refresh_token(refresh_token: str):
         )
 
 
-async def get_current_user(
-    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
-):
+
+async def get_current_user(request: Request, db: Session = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-
+    token_cookie = request.cookies.get("access_token")
+    print(token_cookie)
+    if not token_cookie:
+        raise credentials_exception
     try:
         # Decode JWT
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        if payload["scope"] == "access_token":
-            email = payload["sub"]
-            if email is None:
-                raise credentials_exception
-        else:
+        payload = jwt.decode(token_cookie, SECRET_KEY, algorithms=[ALGORITHM])
+        email = payload.get("sub")
+        if email is None:
             raise credentials_exception
-    except JWTError as e:
+    except JWTError:
         raise credentials_exception
 
     user = await repository_users.get_user_by_email(email, db)
     if user is None:
         raise credentials_exception
     return user
+
 
 
 async def get_admin(user: User=Depends(get_current_user)):
